41f7339c62909153b67dc23086ce897166426989
Svend Tofte <stofte@gmail.com>
2018-09-20 14:29:28 +0200

    More rustiness

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,10 @@ extern crate termion;
 
 use termion::event::{Key, Event};
 use termion::input::{TermRead};
-use termion::raw::IntoRawMode;
-use std::io::{Write, stdout, stdin};
+use termion::raw::{IntoRawMode, RawTerminal};
+use std::io::{Write, Stdin, Stdout};
 
-fn main() {
-    let stdin = stdin();
-    let mut stdout = stdout().into_raw_mode().unwrap();
-
-    write!(stdout,
-           "{}{}q to exit{}",
-           termion::clear::All,
-           termion::cursor::Goto(1, 1),
-           termion::cursor::Hide)
-            .unwrap();
-    stdout.flush().unwrap();
-
+fn main_loop(stdin: Stdin, stdout: &mut RawTerminal<Stdout>) {
     for c in stdin.events() {
         let evt = c.unwrap();
         match evt {
@@ -28,4 +17,18 @@ fn main() {
         }
         stdout.flush().unwrap();
     }
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout().into_raw_mode().unwrap();
+
+    write!(stdout,
+           "{}{}q to exit{}",
+           termion::clear::All,
+           termion::cursor::Goto(1, 1),
+           termion::cursor::Hide)
+            .unwrap();
+    stdout.flush().unwrap();
+    main_loop(stdin, &mut stdout);
 }
