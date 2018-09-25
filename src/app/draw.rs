ae580fd1696c8e5c655d4a9109b4876d4bc7770d
Svend Tofte <stofte@gmail.com>
2018-09-25 18:22:58 +0200

    Fix type decls

--- src/app/draw.rs
+++ src/app/draw.rs
@@ -1,16 +1,22 @@
 use termion::{style, cursor, color};
+use termion::color::{Bg, Fg, Color, Reset, Rgb, Black};
 
-// static bg_white : color::Bg<color::Color> = color::Bg(color::Rgb(255, 255, 255));
-// static bg_reset : color::Bg<color::Color> = color::Bg(color::Reset);
+static bg_white: Bg<Rgb> = Bg(Rgb(255, 255, 255));
+static fg_black: Fg<Black> = Fg(Black);
+static bg_reset: Bg<Reset> = Bg(Reset);
+static fg_white: Fg<Rgb> = Fg(Rgb(255, 255, 255));
+static bg_gray: Bg<Rgb> = Bg(Rgb(205, 205, 205));
+static bg_brand: Bg<Rgb> = Bg(Rgb(255, 150, 50));
+
 
 pub fn header(area_title: &str, area_state: &str, width: u16, height: u16) {
     let right_off = width - area_state.len() as u16 + 1;
     println!("{}{}{}{} {}{}{}{}{}{}{}", 
-        color::Bg(color::Rgb(255, 150, 50)),
-        color::Fg(color::Rgb(255, 255, 255)),
-        "Branch",
-        color::Bg(color::Rgb(255, 255, 255)),
-        color::Fg(color::Black),
+        bg_brand,
+        fg_white,
+        "Branches ",
+        bg_gray,
+        fg_black,
         area_title,
         " ".repeat(width as usize - area_title.len()),
         cursor::Goto(right_off, 1),
