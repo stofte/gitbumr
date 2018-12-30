8a7593f8caa42848b52c01fd473cca57815ba945
Svend Tofte <stofte@gmail.com>
2018-12-30 18:12:14 +0100

    Handle root node diff case (instead of panicking)

--- lib/src/utils.rs
+++ lib/src/utils.rs
@@ -127,71 +127,71 @@ pub fn get_chan_revwalker(path: String, filter: String, max_count: usize) -> Rec
 pub fn parse_diff_parent(commit: &git2::Commit, repo: &Repository) -> (Vec<DiffsItem>, usize) {
     let mut list = vec![];
     let mut max_filename_len = 0;
-    match commit.parent_id(0) {
+    let t = commit.tree().unwrap();
+    let diff = match commit.parent_id(0) {
         Ok(id) => {
-            let t = commit.tree().unwrap();
             let parent_c = repo.find_commit(id).unwrap();
             let parent_tree = repo.find_tree(parent_c.tree_id()).unwrap();
-            let diff = repo.diff_tree_to_tree(Some(&parent_tree), Some(&t), None).unwrap();
-            let delta_cnt = diff.deltas().len();
-            for idx in 0..delta_cnt {
-                let delta = diff.get_delta(idx).unwrap();
-                let delta_old_file = delta.old_file().path().unwrap();
-                let delta_new_file = delta.new_file().path().unwrap();
-                let delta_status = delta.status();
-                let mut patch = git2::Patch::from_diff(&diff, idx).unwrap().unwrap();
-                let mut hunks = vec![];
-                let mut hunks_origins = vec![];
-                let mut hunks_lineno_new = vec![];
-                let mut hunks_lineno_old = vec![];
-                for h_idx in 0..patch.num_hunks() {
-                    let h_lines = patch.num_lines_in_hunk(h_idx).unwrap();
-                    let mut hunk_str = String::with_capacity(300);
-                    let mut hunk_origins_vec = vec![];
-                    let mut hunk_lineno_new_vec = vec![];
-                    let mut hunk_lineno_old_vec = vec![];
-                    for h_line_idx in 0..h_lines {
-                        let h_line = patch.line_in_hunk(h_idx, h_line_idx).unwrap();
-                        // attempt to decode the hunk line as utf8. if this fails, assume it's a binary thing and skip the hunk
-                        match str::from_utf8(h_line.content()) {
-                            Ok(h_line_decoded) => {
-                                hunk_str.push_str(str::from_utf8(h_line.content()).unwrap());
-                                hunk_origins_vec.push(h_line.origin());
-                                hunk_lineno_new_vec.push(h_line.new_lineno());
-                                hunk_lineno_old_vec.push(h_line.old_lineno());
-                            },
-                            Err(..) => {
-                                break;
-                            }
-                        }
-                    }
-                    hunks.push(hunk_str);
-                    hunks_origins.push(hunk_origins_vec);
-                    hunks_lineno_new.push(hunk_lineno_new_vec);
-                    hunks_lineno_old.push(hunk_lineno_old_vec);
-                }
-                let patch_buf = patch.to_buf().unwrap();
-                let mut patch_str = "";
-                match patch_buf.as_str() {
-                    Some(pstr) => patch_str = pstr,
-                    None => ()
-                };
-                let fn_str = format!("{}", pathbuf_to_string(delta_new_file.to_path_buf()));
-                if fn_str.len() > max_filename_len {
-                    max_filename_len = fn_str.len();
-                }
-                list.push(DiffsItem {
-                    filename: fn_str,
-                    status: format!("{:?}", delta_status),
-                    patch: patch_str.to_string(),
-                    hunks: hunks,
-                    lines_origin: hunks_origins,
-                    lines_new: hunks_lineno_new,
-                    lines_old: hunks_lineno_old,
-                });
-            }
+            repo.diff_tree_to_tree(Some(&parent_tree), Some(&t), None).unwrap()
         },
-        Err(..) => panic!("handle root node!")
+        Err(..) => repo.diff_tree_to_tree(None, Some(&t), None).unwrap()
+    };
+    let delta_cnt = diff.deltas().len();
+    for idx in 0..delta_cnt {
+        let delta = diff.get_delta(idx).unwrap();
+        let delta_old_file = delta.old_file().path().unwrap();
+        let delta_new_file = delta.new_file().path().unwrap();
+        let delta_status = delta.status();
+        let mut patch = git2::Patch::from_diff(&diff, idx).unwrap().unwrap();
+        let mut hunks = vec![];
+        let mut hunks_origins = vec![];
+        let mut hunks_lineno_new = vec![];
+        let mut hunks_lineno_old = vec![];
+        for h_idx in 0..patch.num_hunks() {
+            let h_lines = patch.num_lines_in_hunk(h_idx).unwrap();
+            let mut hunk_str = String::with_capacity(300);
+            let mut hunk_origins_vec = vec![];
+            let mut hunk_lineno_new_vec = vec![];
+            let mut hunk_lineno_old_vec = vec![];
+            for h_line_idx in 0..h_lines {
+                let h_line = patch.line_in_hunk(h_idx, h_line_idx).unwrap();
+                // attempt to decode the hunk line as utf8. if this fails, assume it's a binary thing and skip the hunk
+                match str::from_utf8(h_line.content()) {
+                    Ok(h_line_decoded) => {
+                        hunk_str.push_str(str::from_utf8(h_line.content()).unwrap());
+                        hunk_origins_vec.push(h_line.origin());
+                        hunk_lineno_new_vec.push(h_line.new_lineno());
+                        hunk_lineno_old_vec.push(h_line.old_lineno());
+                    },
+                    Err(..) => {
+                        break;
+                    }
+                }
+            }
+            hunks.push(hunk_str);
+            hunks_origins.push(hunk_origins_vec);
+            hunks_lineno_new.push(hunk_lineno_new_vec);
+            hunks_lineno_old.push(hunk_lineno_old_vec);
+        }
+        let patch_buf = patch.to_buf().unwrap();
+        let mut patch_str = "";
+        match patch_buf.as_str() {
+            Some(pstr) => patch_str = pstr,
+            None => ()
+        };
+        let fn_str = format!("{}", pathbuf_to_string(delta_new_file.to_path_buf()));
+        if fn_str.len() > max_filename_len {
+            max_filename_len = fn_str.len();
+        }
+        list.push(DiffsItem {
+            filename: fn_str,
+            status: format!("{:?}", delta_status),
+            patch: patch_str.to_string(),
+            hunks: hunks,
+            lines_origin: hunks_origins,
+            lines_new: hunks_lineno_new,
+            lines_old: hunks_lineno_old,
+        });
     }
     (list, max_filename_len)
 }
